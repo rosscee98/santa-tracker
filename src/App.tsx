66ac6b50478a9acbc91d882dee71ee1913015269
rosscee98 <rosscee1998@gmail.com>
2022-11-21 11:04:46 +0000

    plot route lines on map

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { ComposableMap, Geographies, Geography, Marker } from 'react-simple-maps';
+import { ComposableMap, Geographies, Geography, Marker, Line } from 'react-simple-maps';
 import './App.css';
 import { findTimeUntilNextDestination } from './utils/findTimeUntilNextDestination';
-import locations from './utils/locations.json';
+import locationsJson from './utils/locations.json';
 
 function App() {
   const geoUrl = "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json";
-
   const label = findTimeUntilNextDestination();
+  const locations = Object.entries(locationsJson);
 
   return (
     <div className="App">
       <p>{label}</p>
-      <ComposableMap>
+      <ComposableMap projection="geoMercator" width={1000}>
         <Geographies geography={geoUrl}>
           {
             ({ geographies }) => (
@@ -23,13 +23,14 @@ function App() {
           }
         </Geographies>
         {
-          Object.entries(locations).map(([name, { lat, lng }]) => (
+          locations.map(([name, { lat, lng }]) => (
             <Marker key={name} coordinates={[lng, lat]}>
               <circle r={3} fill="#F53" />
               <text fontSize={8}>{name}</text>
             </Marker>
           ))
         }
+        <Line coordinates={locations.map((location) => [location[1].lng, location[1].lat])} stroke="#f00216" strokeWidth={0.7} />
       </ComposableMap>
     </div>
   );
